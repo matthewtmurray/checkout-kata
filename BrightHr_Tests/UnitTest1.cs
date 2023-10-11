48fd994b89e9fdff7cd18a1d9537e8d49c2891b4
Matthew Murray <matthew.murray@drdoctor.co.uk>
2023-10-11 21:14:12 +0100

    adds initial test

--- BrightHr_Tests/UnitTest1.cs
+++ BrightHr_Tests/UnitTest1.cs
@@ -1,16 +1,38 @@
+using BrightHR_Checkout;
+
 namespace BrightHr_Tests
 {
     public class Tests
     {
+        private Checkout _checkout;
+        private string _singleItemBasket = "A";
+        private List<SkuRule> _skuRules_SomeOffers;
+
         [SetUp]
         public void Setup()
         {
+            _skuRules_SomeOffers = new List<SkuRule>
+            {
+                new SkuRule { Sku = 'A', Price = 50, HasOfferPrice = true, OfferQuantity = 3, OfferPrice = 130 },
+                new SkuRule { Sku = 'B', Price = 30, HasOfferPrice = true, OfferQuantity = 2, OfferPrice = 55 },
+                new SkuRule { Sku = 'C', Price = 20, HasOfferPrice = false, OfferQuantity = 3, OfferPrice = 50 },
+                new SkuRule { Sku = 'D', Price = 15, HasOfferPrice = false, OfferQuantity = 5, OfferPrice = 50 },
+            };
         }
 
         [Test]
-        public void Test1()
+        public void IsValidPrice_SingleItemBasket_SomeOffers()
         {
-            Assert.Pass();
+            var basket = _singleItemBasket;
+            var expectedBasketTotal = 50;
+            _checkout = new Checkout(_skuRules_SomeOffers);
+            foreach (var item in basket.ToCharArray())
+            {
+                _checkout.Scan(item.ToString());
+            }
+
+            var actualBasketTotal = _checkout.GetTotalPrice();
+            Assert.AreEqual(expectedBasketTotal, actualBasketTotal);
         }
     }
 }
