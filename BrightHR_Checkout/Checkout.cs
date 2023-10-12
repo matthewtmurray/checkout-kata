4ff4f47db1094db81da6134142012608077917d5
Matthew Murray <matthew.murray@drdoctor.co.uk>
2023-10-12 09:18:59 +0100

    Fleshed out code to make complete and make tests pass

--- BrightHR_Checkout/Checkout.cs
+++ BrightHR_Checkout/Checkout.cs
@@ -1,28 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BrightHR_Checkout
+﻿namespace BrightHR_Checkout
 {
     public class Checkout : ICheckout
     {
-        private List<SkuRule> _skuRules;
-        private string basketItems = string.Empty;
+        private readonly List<SkuRule> _skuRules;
+        private string _basketItems = string.Empty;
         public Checkout(List<SkuRule> skuRules)
         {
             _skuRules = skuRules;
         }
         public void Scan(string item)
         {
-            basketItems = basketItems + item;
+            _basketItems += item;
         }
 
         public int GetTotalPrice()
         {
             var totalPrice = 0;
-            totalPrice += _skuRules.Where(r => r.Sku == basketItems[0]).Select(r => r.Price).FirstOrDefault();
+            var basketItemsArray = _basketItems.ToCharArray();
+
+            var aCount = basketItemsArray.Count(letter => letter == 'A');
+            var bCount = basketItemsArray.Count(letter => letter == 'B');
+            var cCount = basketItemsArray.Count(letter => letter == 'C');
+            var dCount = basketItemsArray.Count(letter => letter == 'D');
+
+            totalPrice += GetTotalPricePerItem('A', aCount);
+            totalPrice += GetTotalPricePerItem('B', bCount);
+            totalPrice += GetTotalPricePerItem('C', cCount);
+            totalPrice += GetTotalPricePerItem('D', dCount);
+
+            return totalPrice;
+        }
+
+        private int GetTotalPricePerItem(char item, int itemCount)
+        {
+            var totalPrice = 0;
+
+            if (itemCount <= 1 || !_skuRules.Where(r => r.Sku == item).Select(r => r.HasOfferPrice).FirstOrDefault())
+            {
+                totalPrice += _skuRules.Where(r => r.Sku == item).Select(r => r.Price).FirstOrDefault() * itemCount;
+            }
+            else
+            {
+                var offerQuantity = _skuRules.Where(r => r.Sku == item).Select(r => r.OfferQuantity).FirstOrDefault();
+                var offerPrice = _skuRules.Where(r => r.Sku == item).Select(r => r.OfferPrice).FirstOrDefault();
+                var fullPrice = _skuRules.Where(r => r.Sku == item).Select(r => r.Price).FirstOrDefault();
+                var singlePricedItems = itemCount % offerQuantity;
+                var offerQuantityMultiple = (itemCount - singlePricedItems) / offerQuantity;
+                totalPrice += (offerQuantityMultiple * offerPrice) + (singlePricedItems * fullPrice);
+            }
+
             return totalPrice;
         }
     }
